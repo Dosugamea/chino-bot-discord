e01020cbf209e75d3cb8bc6af1400b985abbbeea
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-09-08 01:31:33 +0900

    🎨 Load cogs and config from another files

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
-from discord.ext import commands  # Bot Commands Frameworkをインポート
+from discord.ext import commands
+from cogs.gochiira import GochiiraCog
+from cogs.weather import WeatherCog
 import discord
 import dataclasses
-import requests
-import random
 
 
 @dataclasses.dataclass
 class Config:
-    endpoint: str
-    token: str
-    cdn: str
+    weather_endpoint: str
+    gochiira_endpoint: str
+    gochiira_token: str
+    gochiira_cdn: str
 
 
-# コグとして用いるクラスを定義。
-class TestCog(commands.Cog):
-
-    # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
-    def __init__(self, bot, config):
-        self.bot = bot
-        self.config = config
-
-    @commands.command()
-    async def chino(self, ctx):
-        resp = requests.get(
-            self.config.endpoint, headers={"Authorization": self.config.token}
-        ).json()["data"]["imgs"]
-        resp = random.choice(resp)
-        embed = discord.Embed(
-            title=resp["title"],
-            url=resp["originUrl"],
-            description=resp["artist"]["name"],
-            color=0x7accff
-        )
-        embed.set_thumbnail(
-            url=f"{self.config.cdn}/illusts/large/{resp['illustID']}.jpg"
-        )
-        embed.add_field(name="登録日", value=resp["date"], inline=False)
-        embed.set_footer(text="Provided from Gochiira")
-        await ctx.send(embed=embed)
-
-
-# クラスの定義。ClientのサブクラスであるBotクラスを継承。
 class MyBot(commands.Bot):
-
-    # Botの準備完了時に呼び出されるイベント
     async def on_ready(self):
         print('-----')
         print(self.user.name)
         print(self.user.id)
         print('-----')
+
+    async def on_command_error(self, error):
+        if isinstance(error, commands.CommandNotFound):
+            await self.send("**Invalid command. Try using** `help` **to figure out commands!**")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await self.send('**Please pass in all requirements.**')
+        if isinstance(error, commands.MissingPermissions):
+            await self.send("**You don't have all the requirements or permissions for using this command :angry:**")
+        embed = discord.Embed(title="エラー情報", description="", color=0xf00)
+        embed.add_field(name="エラー発生サーバー名", value=self.guild.name, inline=False)
+        embed.add_field(name="エラー発生サーバーID", value=self.guild.id, inline=False)
+        embed.add_field(name="エラー発生ユーザー名", value=self.author.name, inline=False)
+        embed.add_field(name="エラー発生ユーザーID", value=self.author.id, inline=False)
+        embed.add_field(name="エラー発生コマンド", value=self.message.content, inline=False)
+        embed.add_field(name="発生エラー", value=error, inline=False)
+        await self.send(embed=embed)
 
 
 if __name__ == '__main__':
@@ -58,10 +44,12 @@ if __name__ == '__main__':
 
     TOKEN = os.getenv('BOT_TOKEN')
     config = Config(
-        endpoint=os.getenv('GOCHIIRA_ENDPOINT'),
-        token=os.getenv('GOCHIIRA_TOKEN'),
-        cdn=os.getenv('GOCHIIRA_CDN')
+        weather_endpoint=os.getenv('WEATHER_ENDPOINT'),
+        gochiira_endpoint=os.getenv('GOCHIIRA_ENDPOINT'),
+        gochiira_token=os.getenv('GOCHIIRA_TOKEN'),
+        gochiira_cdn=os.getenv('GOCHIIRA_CDN')
     )
     bot = commands.Bot(command_prefix='!')
-    bot.add_cog(TestCog(bot, config))
+    bot.add_cog(GochiiraCog(bot, config))
+    bot.add_cog(WeatherCog(bot, config))
     bot.run(TOKEN)
